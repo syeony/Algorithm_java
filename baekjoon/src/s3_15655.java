5f7ee7200819537e61222d0516243b6248cb6c17
syeony <fltkfltk1004@naver.com>
2025-02-15 01:34:37 +0900

    0215

--- baekjoon/src/s3_15655.java
+++ baekjoon/src/s3_15655.java
@@ -5,12 +5,16 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class s3_15655 {
+    static int arr[], answer[],m,n;
+    static StringBuilder sb;
+    static boolean visited[];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
@@ -18,10 +22,30 @@ public class s3_15655 {
         }
 
         Arrays.sort(arr);
+        answer = new int[m];
+        visited = new boolean[n];
+        sb = new StringBuilder();
 
+        dfs(0,0);
+        System.out.println(sb);
+    }
 
+    static void dfs(int depth, int start){
+        if(depth>=m){
+            for(int a:answer){
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
 
-
-
+        for(int i=start;i<n;i++){
+            if(!visited[i]){
+                visited[i]=true;
+                answer[depth]=arr[i];
+                dfs(depth+1,i);
+                visited[i]=false;
+            }
+        }
     }
 }
