b38b315317e2fb94f9caf8c6f87d2b43949c3e0d
syeony <fltkfltk1004@naver.com>
2025-02-15 15:05:18 +0900

    0215

---
+++ baekjoon/src/s2_11722.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class s2_11722 {
+    static int n,arr[],answer[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        answer = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        answer[0]=1;
+        update();
+    }
+    public static void update(){
+
+
+        for(int i=1;i<n;i++){
+            int max=0;
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[i]){
+                    max = Math.max(max,answer[j]);
+                }
+            }
+            answer[i]=max+1;
+        }
+        Arrays.sort(answer);
+        System.out.println(answer[n-1]);
+
+    }
+}
