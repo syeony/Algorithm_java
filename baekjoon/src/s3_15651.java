d23d6fc01e26ccba041b20f65cfb7e681b14295e
syeony <fltkfltk1004@naver.com>
2025-03-15 00:28:23 +0900

    0315

---
+++ baekjoon/src/s3_15651.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class s3_15651 {
+    static int n,m;
+    static int[] arr;
+    static boolean[] v;
+    static int[] b;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        arr=new int[n];
+        for(int i=1;i<n+1;i++){
+            arr[i-1]=i;
+        }
+//        System.out.println(Arrays.toString(arr));
+        v=new boolean[n];
+        b=new int[m];
+        perm(0);
+    }
+
+    static void perm(int idx){
+        if(idx==m){
+            for(int i=0;i<m;i++){
+                System.out.print(b[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0;i<n;i++){
+//            if(!v[i]){
+//                v[i]=true;
+                b[idx]=arr[i];
+                perm(idx+1);
+//                v[i]=false;
+//            }
+        }
+    }
+}
